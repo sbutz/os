68ae840eafd42aa61eb5dcb4a6e2e191bce59bea
Stefan Butz <stefan260699@gmail.com>
2022-01-28 21:32:59 +0100

    Blatt 10

---
+++ Blatt10/concurrency/ThreadDemo.java
@@ -0,0 +1,44 @@
+/* j.n.magee
+//updated: daniel.sykes 2013
+ */
+
+package concurrency;
+
+import java.awt.*;
+import java.applet.*;
+import concurrency.display.*;
+
+
+class Rotator implements Runnable {
+
+  public void run() {
+    try {
+      while(true) ThreadPanel.rotate();
+    } catch(InterruptedException e) {}
+  }
+}
+
+
+public class ThreadDemo extends Applet {
+  ThreadPanel A;
+  ThreadPanel B;
+
+  public void init() {
+    A = new ThreadPanel("Thread A",Color.blue);
+    B = new ThreadPanel("Thread B",Color.blue);
+    add(A);
+    add(B);
+	setBackground(Color.lightGray);
+  }
+
+  public void start() {
+    A.start(new Rotator());
+    B.start(new Rotator());
+  }
+
+  public void stop() {
+    A.stop();
+    B.stop();
+  }
+}
+
